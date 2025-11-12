e3c069882388686e1e6624a3fb63d5a43aafd4ff
88863-Vector-Hogo <logi.farst.leno@gmail.com>
2025-11-12 20:34:13 +0330

    Welco2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Button from './Component/Button'
 import Desc from './Component/BoxPage'
 import Footer from './Component/Footer'
 function App() {
+
+  function ClickedSite() {
+    window.location.href = "https://hamyar-page.vercel.app/";
+  }
   return (
     <>
 
@@ -21,8 +25,8 @@ function App() {
           <h1 className="text-6xl font-bold text-0118D8 mb-6 text-center">همیار فنی</h1>
           <p className='text-2xl font-bold text-black'>همیار فنی همراه همیشه تو در کار فنی</p>
           <div className='mt-4 flex justify-around'>
-            <Button text="سایت"></Button>
-            <Button text="اپلیکیشن"></Button>
+            <Button text="سایت" onClick={ClickedSite}></Button>
+            <Button text="اپلیکیشن" onClick={ClickedSite}></Button>
           </div>
         </div>
       </div>
@@ -33,6 +37,7 @@ function App() {
         title="همیار فنی چیست؟" 
         desc=" همیار فنی یک اپلیکیشن تحت وب سبک و حرفه‌ای است که مخصوص کارهای فنی و مهندسی طراحی شده است. این اپ نه تنها برای مهندسان و تکنسین‌ها، بلکه برای افراد عادی هم قابل استفاده است. امکانات آن شامل دوره‌های آموزشی فنی و هوش مصنوعی برای تعمیرکاران جهت تشخیص قطعات، بخش فروش آنلاین قطعات و ابزارها، و ابزارهای کاربردی دیگر برای مدیریت و یادگیری فنی می‌باشد. همچنین خدمات در محل" 
         srcI = "/8259.jpg"
+
       ></Desc>
     </div>
 
--- src/Component/Button.jsx
+++ src/Component/Button.jsx
@@ -1,6 +1,6 @@
-export default function Button({text}) {
+export default function Button({text, onClick}) {
     return(
-        <button className="bg-[#1B56FD] hover:bg-[#3458FF] text-white font-bold py-3 px-8 rounded-2xl shadow-lg transition-all duration-300 transform hover:scale-105 cursor-pointer">
+        <button className="bg-[#1B56FD] hover:bg-[#3458FF] text-white font-bold py-3 px-8 rounded-2xl shadow-lg transition-all duration-300 transform hover:scale-105 cursor-pointer" onClick={onClick}>
             {text}
         </button>
 
